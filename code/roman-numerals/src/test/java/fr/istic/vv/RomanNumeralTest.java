11c9a2626c2f630e7bdced78d3acde8082d89815
Iziny <yoanndewilde@gmail.com>
2024-01-24 14:46:22 +0100

    Write some property tests
    * fromRoman(toRoman(int)) == int
    * int > 3999 => exception
    * int < 0 => exception

--- code/roman-numerals/src/test/java/fr/istic/vv/RomanNumeralTest.java
+++ code/roman-numerals/src/test/java/fr/istic/vv/RomanNumeralTest.java
@@ -1,9 +1,24 @@
 package fr.istic.vv;
+
 import net.jqwik.api.*;
+import net.jqwik.api.constraints.IntRange;
+import static org.junit.jupiter.api.Assertions.*;
+
+import static fr.istic.vv.RomanNumeraUtils.*;
 
 public class RomanNumeralTest {
     @Property
-    boolean absoluteValueOfAllNumbersIsPositive(@ForAll int anInteger) {
-        return Math.abs(anInteger) >= 0;
+    boolean arabicToRomanToArabicKeepsNumber(@ForAll @IntRange(min = 0, max = 3999) int i) {
+        return parseRomanNumeral(toRomanNumeral(i)) == i;
+    }
+
+    @Property
+    void tooSmallNumberThrowsException(@ForAll @IntRange(min = Integer.MIN_VALUE, max = -1) int i) {
+        assertThrows(RuntimeException.class, () -> toRomanNumeral(i));
+    }
+
+    @Property
+    void tooBigNumberThrowsException(@ForAll @IntRange(min = 4000, max = Integer.MAX_VALUE) int i) {
+        assertThrows(RuntimeException.class, () -> toRomanNumeral(i));
     }
 }
